dc89e9293f0460016eead480a90f3798ca74ffc8
selenoid <selenoid@gmail.com>
2016-03-14 00:33:36 +0200

    changed business logic
    modification of randomization rules.
    No more than one previous symbol on one roll. (ie. 7 and star);

--- slot.bat
+++ slot.bat
@@ -1,7 +1,7 @@
 @echo off
 echo.
 
-set NodePackagesPath=C:\DEV\node\test // This is my path, you can edit them
+set NodePackagesPath=C:\DEV\node\node-test // This is my path, you can edit them
 
 set Path=%NodePackagesPath%\node_modules\.bin;%PATH%
 set Path=%NodePackagesPath%;%PATH%
@@ -13,5 +13,6 @@ echo Environment variables are successfully added.
 echo. 
 echo. 
 echo. 
+
 
 node slot.js
